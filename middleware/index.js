2db03ceaac24c152ccb7d9d9598949f938d228f5
Aliecake <neyabenz@gmail.com>
2020-04-24 16:11:40 -0400

    authentication

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,11 @@
-const express = require('express')
+const express = require('express');
+const auth = require('basic-auth');
+const bcryptjs = require('bcryptjs');
+const { User } = require('../models')
 
 
 const middleware = {}
+
 
 /* Handler function to wrap each route. */
  middleware.asyncHandler = (cb) => {
@@ -11,6 +15,46 @@ const middleware = {}
       } catch(error){
         res.status(500).send(error);
       }
+    }
+  }
+
+  middleware.authenticateUser = async(req, res, next) => {
+    let message = null;
+    let user = null;
+    //parsed from Auth header
+    var credentials = auth(req)
+  
+
+    if (credentials) {
+         user = await User.findOne({
+          where: {
+            emailAddress: credentials.name
+          }
+        })
+
+      if (user) {
+        //check password
+        const authenticated = bcryptjs.compareSync(credentials.pass, user.password)
+
+        if (authenticated) {
+          //store in req
+          req.currentUser = user
+        } else {
+          message = `Authentication failed for username: ${user.emailAddress}`
+        }
+      } else {
+        message = `User not found for username: ${credentials.name}`
+      }
+    } else {
+      message = `Auth header not found`
+    }
+    if (message) {
+      console.warn(message)
+      res.status(401).json({
+        message: `Access denied`
+      })
+    } else {
+      next();
     }
   }
 
--- routes/users.js
+++ routes/users.js
@@ -2,42 +2,43 @@
 
 const express = require('express');
 const middleware = require('../middleware')
-const { Course, User } = require('../models')
+const { User } = require('../models')
+const auth = require('basic-auth');
+const bcryptjs = require('bcryptjs');
 //express validator
 const router = express.Router();
 
+
 /**
- * GET /api/users 200 - Returns the currently authenticated user
+ * GET /api/users 200 - Returns the currently authenticated user - Auth through middleware
  */
-router.get('/users', async(req, res) => {
-    console.log(`Hello`)
+router.get('/users', middleware.authenticateUser, middleware.asyncHandler(async(req, res) => {
+    const currentUser = req.currentUser;
     
-    try {
+    const user = await User.findOne({
+        where: {
+            firstName: currentUser.firstName,
+            emailAddress: currentUser.emailAddress
+        }
+    });
 
-        const user = await User.findAll()
-        res.status(200).json({
-            message: user
-        })
-        console.log(user)
-    } catch(error) {
-        console.log(error)
-    }
-    
-   
-
-});
+    res.status(200).json({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        emailAddress: user.emailAddress,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt
+    })
+}));
 /**
  * POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
  */
-//firstName, lastName, emailAddress, password
 
 router.post('/users', middleware.asyncHandler(async(req, res) => {
-
-    
-    const user = await User.create(req.body)
-    res.status(200).json({
-        message:  user
-    })
+    const user = req.body;
+    user.password = bcryptjs.hashSync(user.password)
+    await User.create(user)
+    res.location('/').status(201).end()
 }));
 
 
