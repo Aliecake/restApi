5884f340ab4abc3e56942fd5613215760bfd7855
Aliecake <neyabenz@gmail.com>
2020-04-25 16:17:04 -0400

    express validation

---
+++ middleware/validators.js
@@ -0,0 +1,25 @@
+const { check, validationResult } = require('express-validator');
+
+validation = {}
+
+validation.user = [
+    check('firstName')
+        .isLength({ min:1 }).withMessage('First Name is a required field.')
+        .isAlphanumeric().withMessage('Login must be alphanumeric.'),
+    check('lastName')
+        .isLength({ min:1 }).withMessage('First Name is a required field.')
+        .isAlphanumeric().withMessage('Login must be alphanumeric.'),
+    check('emailAddress')
+        .isEmail().withMessage(`Email must be formatted email@email.com`),
+    check('password')
+        .isLength({ min:4 }).withMessage('Password must be at least 4 characters in length.')
+]
+
+validation.course = [
+    check('title')
+        .isLength({ min:1 }).withMessage('Title required field.'),
+    check('description')
+        .isLength({ min:1 }).withMessage('Description is a required field.')
+]
+
+module.exports = validation
